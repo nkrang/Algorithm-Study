4e3496f741826df297570701785d770d8b502568
SeongEun <55864257+nkrang@users.noreply.github.com>
2021-07-28 22:23:17 +0900

    [ADD] 0728

---
+++ B-10828/스택.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+
+for _ in range(n):
+    command = input().strip('\n').split()
+    if command[0] == "push":
+        stack.append(int(command[1]))
+    elif command[0] == "pop":
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == "size":
+        print(len(stack))
+    elif command[0] == "empty":
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == "top":
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
