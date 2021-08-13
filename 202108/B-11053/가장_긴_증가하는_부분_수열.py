75a0434d1ed1979e554f8beb2a934d38adcbb4ac
SeongEun <55864257+nkrang@users.noreply.github.com>
2021-08-13 18:52:46 +0900

    [ADD] 0813

---
+++ 202108/B-11053/가장_긴_증가하는_부분_수열.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+numbers = list(map(int, input().split()))
+
+dp = []
+for i in range(n):
+    if i == 0:
+        dp.append(1)
+    else:
+        large = 0
+        for j in range(i):
+            if numbers[j] < numbers[i] and dp[j] > large:
+                large = dp[j]
+        dp.append(large + 1)
+print(max(dp))
