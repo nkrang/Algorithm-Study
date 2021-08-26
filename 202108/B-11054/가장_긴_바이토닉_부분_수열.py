6f8d51a6a3f45fc2b1e71a11084946c4cacb1fc3
SeongEun <55864257+nkrang@users.noreply.github.com>
2021-08-26 15:16:07 +0900

    [ADD] 0826

---
+++ 202108/B-11054/가장_긴_바이토닉_부분_수열.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+numbers = list(map(int, input().split()))
+
+forward = [1] * n
+reverse = [1] * n
+both = []
+
+for i in range(n):
+    for j in range(i):
+        if numbers[j] < numbers[i]:
+            forward[i] = max(forward[i], forward[j] + 1)
+    both.append(forward[i])
+
+print(both)
+
+#역방향으로 for문을 돌릴 때는 -1을 써줘야 도는 구낭,,,,,,,,
+for i in range(n-1, -1, -1):
+    for j in range(n-1, i-1, -1):
+        if numbers[j] < numbers[i]:
+            reverse[i] = max(reverse[i], reverse[j] + 1)
+    both[i] += reverse[i]
+
+print(max(both) - 1)
