a5a06e367c3a89b87f5e5519530a05e5514a24a0
SeongEun <55864257+nkrang@users.noreply.github.com>
2021-09-05 00:25:48 +0900

    [ADD] 0904

---
+++ 202109/P-42576/완주하지_못한_선수.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    else:
+        return participant[-1]
