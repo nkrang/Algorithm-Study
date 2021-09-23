55c911d323fe1ea7ffba49bad4bf1e1f673ca750
SeongEun <55864257+nkrang@users.noreply.github.com>
2021-09-23 14:21:14 +0900

    [ADD] 0922

---
+++ 202109/B-2293/동전1.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coin = []
+for _ in range(n):
+    coin.append(int(input()))
+
+dp = [0] * (k+1)
+dp[0] = 1
+
+for i in coin:
+    for j in range(i, k+1):
+        dp[j] += dp[j-i]
+
+print(dp[k])
