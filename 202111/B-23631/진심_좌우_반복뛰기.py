2d54c0cdf3b8f1acb02f75686e135a4b7e9bd78d
SeongEun <55864257+nkrang@users.noreply.github.com>
2021-11-28 00:40:17 +0900

    [ADD] 1126

---
+++ 202111/B-23631/진심_좌우_반복뛰기.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N, K = map(int, input().split())
+    if N <= K :
+        print(N-1, 'R')
+        continue
+    left, right = 1, N//K
+    jump = 0
+    dist = 0
+
+    while left <= right:
+        mid = (left + right) // 2
+        temp = mid * (mid + 1) * K // 2
+        if temp <= N-1:
+            jump = max(mid, jump)
+            dist = N - 1 - temp
+            left = mid+1
+        else:
+            right = mid-1
+
+    loc = 0
+    direction = 0
+    result = 0
+
+    if jump % 2 != 0:
+        loc = K * (jump // 2 + 1)
+        direction = "L"
+        result = loc - dist
+    else:
+        loc = K * (jump // 2) * -1
+        direction = "R"
+        result = loc + dist
+
+    print(result, direction)
+
+    
