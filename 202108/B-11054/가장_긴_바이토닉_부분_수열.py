7c8b53945f9741514193af571589eae13c5a76ba
SeongEun <55864257+nkrang@users.noreply.github.com>
2021-12-17 20:51:55 +0900

    [ADD] 1217

--- 202108/B-11054/가장_긴_바이토닉_부분_수열.py
+++ 202108/B-11054/가장_긴_바이토닉_부분_수열.py
@@ -14,7 +14,6 @@ for i in range(n):
             forward[i] = max(forward[i], forward[j] + 1)
     both.append(forward[i])
 
-#역방향으로 for문을 돌릴 때는 -1을 써줘야 도는 구낭,,,,,,,,
 for i in range(n-1, -1, -1):
     for j in range(n-1, i-1, -1):
         if numbers[j] < numbers[i]:
