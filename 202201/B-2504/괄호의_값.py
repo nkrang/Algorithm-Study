6a3caf3fe7570fd0e366f65ff577cff55e9ecb57
SeongEun <55864257+nkrang@users.noreply.github.com>
2022-01-02 00:30:23 +0900

    [ADD] 0101

---
+++ 202201/B-2504/괄호의_값.py
@@ -0,0 +1,33 @@
+line = list(map(str, input().strip('\n')))
+stack = []
+
+answer = 0
+temp = 1
+for i in range(len(line)):
+    if line[i] == '(':
+        stack.append(line[i])
+        temp *= 2
+    elif line[i] == '[':
+        stack.append(line[i])
+        temp *= 3
+    elif line[i] == ')':
+        if stack and stack[-1] == '(':
+            if line[i-1] == '(':
+                answer += temp
+        else:
+            answer = 0
+            break
+        temp //= 2
+        stack.pop()
+
+    elif line[i] == ']':
+        if stack and stack[-1] == '[':
+            if line[i-1] == '[':
+                answer += temp
+        else:
+            answer = 0
+            break
+        temp //= 3
+        stack.pop()
+            
+print(answer)
